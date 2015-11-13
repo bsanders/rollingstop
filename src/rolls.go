4da8fadd8ecf88f83671813695f808be7de8781b
Bill Sanders <billysanders@gmail.com>
2015-11-12 22:07:00 -0800

    Added query parameters to allow for rolling multiple dice and/or dice with modifiers

--- src/rolls.go
+++ src/rolls.go
@@ -5,6 +5,8 @@ import "time"
 type Roll struct {
     Result      int       `json:"result"`
     DieNumber   int       `json:"dienumber"`
+    Count       int       `json:"count"`
+    Modifier    int       `json:"modifier"`
     Date        time.Time `json:"date"`
 }
 
